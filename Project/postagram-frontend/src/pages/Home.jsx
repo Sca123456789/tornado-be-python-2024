9aa349216bca116880c1742ebafca7a606251643
tornado-02 <tornado-02@ADMIN-02s-iMac.local>
2024-03-18 14:44:01 +0800

    add: postagram-backend

--- Project/postagram-frontend/src/pages/Home.jsx
+++ Project/postagram-frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className=" w-full border-2 h-20  mr-auto">tftfftfftyf</div>
+    <div className=" w-full border-2 h-20  mr-auto">  <h1>Posragram</h1>  </div>
     );
 }

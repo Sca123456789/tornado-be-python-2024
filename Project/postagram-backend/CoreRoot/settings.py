ca55fc49dc023497436c9d7e5fa97786a411b2b5
tornado-02 <tornado-02@ADMIN-02s-iMac.local>
2024-03-18 14:39:56 +0800

    add: gunicorn

--- Project/postagram-backend/CoreRoot/settings.py
+++ Project/postagram-backend/CoreRoot/settings.py
@@ -178,6 +178,6 @@ DEFAULT_AVATAR_URL = "https://api.dicebear.com/7.x/pixel-art/svg"
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://localhost:5174",
+    "http://127.0.0.1:5174"
 ]
